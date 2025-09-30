585b0da21aaca3cc1ca3129eab30d29fe4d0fdc0
Miles Padot <155395178+mpadot@users.noreply.github.com@users.noreply.github.com>
2025-09-30 17:54:32 +0000

    added stuff to sample.py, and I also made tests for test_sample.py

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -1,6 +1,31 @@
 import pytest
 from presidio_anonymizer.sample import sample_run_anonymizer
 
+
 def test_sample_run_anonymizer():
     # replace the following line with your test
-    pass
+
+    text = "My name is Bond."
+    start = 11
+    end = 15
+    new_value = "BIP"
+
+    # Expected
+    expected_text = "My name is BIP."
+    expected_items = [
+        {'start': 11, 'end': 14, 'entity_type': 'PERSON', 'text': 'BIP', 'operator': 'replace'}
+    ]
+
+    
+
+    result = sample_run_anonymizer(text, start, end, new_value)
+
+    print(result.items)
+   
+
+   
+    
+
+
+    
+    
