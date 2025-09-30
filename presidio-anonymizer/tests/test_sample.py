ce780f811b2f757915ec9aa44aba47c051ae0e0e
Miles Padot <155395178+mpadot@users.noreply.github.com@users.noreply.github.com>
2025-09-30 23:54:33 +0000

    Changed and fixed part 3 of lab

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -1,26 +1,32 @@
 import pytest
 from presidio_anonymizer.sample import sample_run_anonymizer
-
-
-def test_sample_run_anonymizer():
-    # replace the following line with your test
-
-    text = "My name is Bond."
-    start = 11
-    end = 15
-    new_value = "BIP"
-
-    # Expected
-    expected_text = "My name is BIP."
-    expected_items = [
-        {'start': 11, 'end': 14, 'entity_type': 'PERSON', 'text': 'BIP', 'operator': 'replace'}
+@pytest.mark.parametrize(
+    "text, start, end, new_value",
+    [
+        ("My name is Bond.", 11, 15, "BIP")
     ]
+)
 
-    
 
+
+def test_sample_run_anonymizer(text, start, end, new_value):
+    # Run function
     result = sample_run_anonymizer(text, start, end, new_value)
 
-    print(result.items)
+    # Expected text after anonymization
+    expected_text = "My name is BIP."
+
+    # Assertions
+    assert result.text == expected_text
+    assert len(result.items) == 1
+
+    item = result.items[0]
+    assert item.start == 11
+    assert item.end == 14   
+    assert item.text == "BIP"
+    assert item.operator == "replace"
+    assert item.entity_type == "PERSON"
+
    
 
    
