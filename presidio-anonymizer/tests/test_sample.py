118f03a9db098f38bc237d9d53f53c11ce118747
Miles Padot <155395178+mpadot@users.noreply.github.com@users.noreply.github.com>
2025-10-01 00:10:03 +0000

    Now this time i fixed test_sample.py properly hopefully

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -1,14 +1,13 @@
+from unittest import result
 import pytest
 from presidio_anonymizer.sample import sample_run_anonymizer
+
 @pytest.mark.parametrize(
     "text, start, end, new_value",
     [
         ("My name is Bond.", 11, 15, "BIP")
     ]
 )
-
-
-
 def test_sample_run_anonymizer(text, start, end, new_value):
     # Run function
     result = sample_run_anonymizer(text, start, end, new_value)
@@ -22,7 +21,7 @@ def test_sample_run_anonymizer(text, start, end, new_value):
 
     item = result.items[0]
     assert item.start == 11
-    assert item.end == 14   
+    assert item.end == 14
     assert item.text == "BIP"
     assert item.operator == "replace"
     assert item.entity_type == "PERSON"
