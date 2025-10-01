e9d4b965b8b07a808f9bb94ee09365524049041b
Miles Padot <155395178+mpadot@users.noreply.github.com@users.noreply.github.com>
2025-10-01 19:36:48 +0000

    fixed

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -6,21 +6,16 @@ def test_sample_run_anonymizer():
     start, end = 11, 15
     engine_result = sample_run_anonymizer(text, start, end)
 
-    # Check the anonymized text
-
-
+    # Check anonymized text
     assert engine_result.text == "My name is BIP."
 
-
-    # Check there is exactly 1 item in results
+    # Check there is exactly 1 item
     assert len(engine_result.items) == 1
     item = engine_result.items[0]
 
     # Check item details
     assert item.start == start
-
-    assert item.end == end
+    assert item.end == end - 1   
     assert item.entity_type == "PERSON"
     assert item.text == "BIP"
     assert item.operator == "replace"
-
