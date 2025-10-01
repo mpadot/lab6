e866ca16181f06744163a2618614d612e67e4898
Miles Padot <155395178+mpadot@users.noreply.github.com@users.noreply.github.com>
2025-10-01 04:20:46 +0000

    fixed

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -1,25 +1,21 @@
-from unittest import result
 import pytest
 from presidio_anonymizer.sample import sample_run_anonymizer
 
 def test_sample_run_anonymizer():
-    text = "My name is Bond"
+    text = "My name is Bond."
     start, end = 11, 15
     engine_result = sample_run_anonymizer(text, start, end)
 
-    assert engine_result.text == "My name is BIP"
+    # Check the anonymized text
+    assert engine_result.text == "My name is BIP."
+
+    # Check there is exactly 1 item in results
     assert len(engine_result.items) == 1
     item = engine_result.items[0]
+
+    # Check item details
     assert item.start == start
-    assert item.end == end - 1
+    assert item.end == end
     assert item.entity_type == "PERSON"
     assert item.text == "BIP"
     assert item.operator == "replace"
-   
-
-   
-    
-
-
-    
-    
