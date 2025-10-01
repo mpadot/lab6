2dfeb50bee8f2c7a2971929ae7218688d3dbf06b
Miles Padot <155395178+mpadot@users.noreply.github.com@users.noreply.github.com>
2025-10-01 03:56:52 +0000

    fixed test_sample.py

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -2,30 +2,19 @@ from unittest import result
 import pytest
 from presidio_anonymizer.sample import sample_run_anonymizer
 
-@pytest.mark.parametrize(
-    "text, start, end, new_value",
-    [
-        ("My name is Bond.", 11, 15, "BIP")
-    ]
-)
-def test_sample_run_anonymizer(text, start, end, new_value):
-    # Run function
-    result = sample_run_anonymizer(text, start, end, new_value)
+def test_sample_run_anonymizer():
+    text = "My name is Bond"
+    start, end = 11, 15
+    engine_result = sample_run_anonymizer(text, start, end)
 
-    # Expected text after anonymization
-    expected_text = "My name is BIP."
-
-    # Assertions
-    assert result.text == expected_text
-    assert len(result.items) == 1
-
-    item = result.items[0]
-    assert item.start == 11
-    assert item.end == 14
+    assert engine_result.text == "My name is BIP"
+    assert len(engine_result.items) == 1
+    item = engine_result.items[0]
+    assert item.start == start
+    assert item.end == end - 1
+    assert item.entity_type == "PERSON"
     assert item.text == "BIP"
     assert item.operator == "replace"
-    assert item.entity_type == "PERSON"
-
    
 
    
